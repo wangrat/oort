4cb26cdccbff542998df0e54181348226e7b18ba
Finley Warman <finleywarman@hotmail.co.uk>
2018-12-08 18:47:21 +0000

    YOOT INTO THE CLOUD

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var apiRouter = require('./routes/api/users')
+var roomsRouter = require('./routes/api/rooms')
 
 var app = express();
 
@@ -22,8 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api/users', apiRouter);
+app.use('/api/rooms', roomsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/api/rooms.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+const doAsync = require('doasync');
+
+// routes will go here
+router.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+
+    var room_id = req.param('id');
+
+    var songData =
+    {
+        "4": [
+            { name: "despacito", artist: "spanish man", votes: 15 },
+            { name: "screaming in the eternal void", artist: "me", votes: -1 },
+            { name: "pls stop calling me 'daddy'", artist: "kolling", votes: 3 }
+        ],
+        "0": [
+            { name: "peep", artist: "lettuce man", votes: 15 },
+            { name: "asdlj", artist: "me", votes: -1 },
+            { name: "pls stop calling me 'daddy'", artist: "kolling", votes: 3 }
+        ]
+    };
+
+    /*
+    saveDictToPublicFolder(songData, function (err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("Saved File");
+    });
+    */
+    /*
+    
+      if (room_id) {
+        res.send(JSON.stringify(songData, null, 4));
+      }
+      else {
+        res.send("Send a room ID first pls.")
+      }
+      //res.send(user_id + '<br>' + token + '\n' + geo);
+      getSongDataByRoomId(room_id);
+    
+      */
+
+    if (room_id) {
+        res.send(getSongDataByRoomId(room_id))
+    }
+    else {
+        res.send("");
+    }
+
+});
+
+function saveDictToPublicFolder(fileData, callback) {
+    fs.writeFile('./public/file.json', JSON.stringify(fileData, null, 4), callback);
+
+}
+
+var songData;
+function getSongDataByRoomId(roomID) {
+    //fs.readFile('./public/file.json', handleFile);
+    //var songData;
+    /*
+    doAsync(fs).readFile('./public/file.json')
+      .then((data) => console.log(JSON.parse(data)));
+      */
+
+    doAsync(fs).readFile('./public/file.json')
+        .then((data) => songData = JSON.parse(data));
+
+
+    console.log(songData);
+    console.log(roomID);
+    if (songData[roomID]) {
+        return songData[roomID];
+    }
+
+
+    //return songData;
+}
+
+module.exports = router;
