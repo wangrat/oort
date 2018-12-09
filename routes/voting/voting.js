25b1039027601cfd537383976517e3cf660161c8
Adam Wangrat <amvangradt@gmail.com>
2018-12-09 02:38:13 +0000

    Add files via upload

--- routes/voting/voting.js
+++ routes/voting/voting.js
@@ -4,9 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-    var room_id = req.param('id');
 
-    res.render('voting', { title: 'Oort', roomID: room_id, songLists: [0] });
 
 
 });
