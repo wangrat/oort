091ab063b5ef1d5141f4d7325f0b11bf16d87ce2
Finley Warman <finleywarman@hotmail.co.uk>
2018-12-09 11:00:53 +0000

    commit

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ var rooms = {};
 
 var app = express();
 
+app.use(express.static("public"));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
