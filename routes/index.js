e03d6e3a28e34e78181232d90f0b2c925ce24c7f
Adam Wangrat <wangrat@localhost.localdomain>
2018-12-09 11:49:07 +0000

    heroku update

--- routes/index.js
+++ routes/index.js
@@ -65,7 +65,7 @@ router.get('/spotify', function (req, res, next) {
 
     var credentials = {
         clientId: '5f2ea699b7a548f68001465a874ef9f0',
-        redirectUri: 'http://' + req.hostname + ':' + req.socket.localPort + '/playlists'
+        redirectUri: 'http://' + req.hostname + '/playlists'
     };
 
     spotify.spotifyApi = new SpotifyWebApi(credentials);
