0dbcb7bb5ad4af31310ca123a6ab85c3d41a916b
Finley Warman <finleywarman@hotmail.co.uk>
2018-12-10 23:30:56 +0000

    Redirect on Credential Failure
    Redirect to Index (Home) page if credentials fail, i.e. by timing out or opening playlists page with token after server restarts.

--- routes/index.js
+++ routes/index.js
@@ -9,173 +9,181 @@ var spotify = require('./spotify.js')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var name = storage.name;
-    console.log(name);
-    storage.name = "changed"
-    console.log(name);
+  var name = storage.name;
+  console.log(name);
+  storage.name = "changed"
+  console.log(name);
 
-    res.render('index', {title: 'Oort'});
+  res.render('index', { title: 'Oort' });
 });
 
 router.get('/playlists', function (req, res, next) {
-    var code = req.query["code"];
+  var code = req.query["code"];
 
+  try {
     spotify.spotifyApi.setCredentials({
-        clientSecret: '80fba228fc5b4a8397f2462ea6f7cbf8',
-        clientId: '5f2ea699b7a548f68001465a874ef9f0'
+      clientSecret: '80fba228fc5b4a8397f2462ea6f7cbf8',
+      clientId: '5f2ea699b7a548f68001465a874ef9f0'
     });
+  }
+  catch{
+    res.redirect('/');
+  }
+
+  // Retrieve an access token and a refresh token
+  spotify.spotifyApi.authorizationCodeGrant(code).then(
+    function (data) {
+      console.log('The token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+      console.log('The refresh token is ' + data.body['refresh_token']);
+
+      // Set the access token on the API object to use it in later calls
+      spotify.spotifyApi.setAccessToken(data.body['access_token']);
+      spotify.spotifyApi.setRefreshToken(data.body['refresh_token']);
+
+      spotify.spotifyApi.getUserPlaylists()
+        .then(function (data) {
+          console.log('Retrieved playlists', data.body);
+
+          for (var item in data.body) {
+            console.log("Item:", item);
+          }
+
+          res.render('playlists', { title: 'Playlists', names: data.body.items });
+        }, function (err) {
+          console.log('Something went wrong!', err);
+          res.redirect('/');
+        });
 
 
-    // Retrieve an access token and a refresh token
-    spotify.spotifyApi.authorizationCodeGrant(code).then(
-        function (data) {
-            console.log('The token expires in ' + data.body['expires_in']);
-            console.log('The access token is ' + data.body['access_token']);
-            console.log('The refresh token is ' + data.body['refresh_token']);
-
-            // Set the access token on the API object to use it in later calls
-            spotify.spotifyApi.setAccessToken(data.body['access_token']);
-            spotify.spotifyApi.setRefreshToken(data.body['refresh_token']);
-
-            spotify.spotifyApi.getUserPlaylists()
-                .then(function (data) {
-                    console.log('Retrieved playlists', data.body);
-
-                    res.render('playlists', {title: 'Playlists', names: data.body.items});
-                }, function (err) {
-                    console.log('Something went wrong!', err);
-                    res.redirect('/');
-                });
-
-
-        },
-        function (err) {
-            console.log('Something went wrong!', err);
-            res.redirect('/');
-        }
-    );
+    },
+    function (err) {
+      console.log('Something went wrong!', err);
+      res.redirect('/');
+    }
+  );
 
 
 });
 
 router.get('/spotify', function (req, res, next) {
-    //res.render('index', {title: 'Oort'});
+  //res.render('index', {title: 'Oort'});
 
-    var scopes = ['playlist-read-private', 'playlist-modify-private', 'playlist-modify-public', 'user-read-playback-state'];
+  var scopes = ['playlist-read-private', 'playlist-modify-private', 'playlist-modify-public', 'user-read-playback-state'];
 
-    var credentials = {
-        clientId: '5f2ea699b7a548f68001465a874ef9f0',
-        redirectUri: 'http://' + req.hostname + ':' + req.socket.localPort + '/playlists'
-    };
+  var credentials = {
+    clientId: '5f2ea699b7a548f68001465a874ef9f0',
+    redirectUri: 'http://' + req.hostname + ':' + req.socket.localPort + '/playlists'
+  };
 
-    spotify.spotifyApi = new SpotifyWebApi(credentials);
+  spotify.spotifyApi = new SpotifyWebApi(credentials);
 
-    // Create the authorization URL
-    var authorizeURL = spotify.spotifyApi.createAuthorizeURL(scopes);
+  // Create the authorization URL
+  var authorizeURL = spotify.spotifyApi.createAuthorizeURL(scopes);
 
-    // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
-    res.redirect(authorizeURL);
+  // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+  res.redirect(authorizeURL);
 });
 
 router.get('/playlists/:id', function (req, res, next) {
-    spotify.spotifyApi.getPlaylist(req.params.id)
-        .then(function (data) {
-            console.log('Some information about this playlist', data.body.tracks.items[0]);
+  spotify.spotifyApi.getPlaylist(req.params.id)
+    .then(function (data) {
+      console.log('Some information about this playlist', data.body.tracks.items[0]);
 
-            var forStorage = {}
+      var forStorage = {}
 
-            data.body.tracks.items.forEach(song => {
-                forStorage[song.track.id] = {
-                    "name": song.track.name,
-                    "artist": song.track.artists[0].name,
-                    "votes": 0,
-                    "playlistsID": data.body.id,
-                    "songID": song.track.id
-                }
-            })
+      data.body.tracks.items.forEach(song => {
+        forStorage[song.track.id] = {
+          "name": song.track.name,
+          "artist": song.track.artists[0].name,
+          "votes": 0,
+          "playlistsID": data.body.id,
+          "songID": song.track.id
+        }
+      })
 
-            var id = Math.floor(100000 + Math.random() * 900000).toString();
+      var id = Math.floor(100000 + Math.random() * 900000).toString();
 
-            storage.rooms[id] = forStorage;
+      storage.rooms[id] = forStorage;
 
 
-            /*
-            saveDictToPublicFolder(roomObject, function (err) {
-              if (err) {
-                console.log(err);
-                return;
-              }
-              console.log("Saved File");
-            });
-            */
+      /*
+      saveDictToPublicFolder(roomObject, function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Saved File");
+      });
+      */
 
-            res.redirect('/rooms/' + id)
+      res.redirect('/rooms/' + id)
 
-        }, function (err) {
-            console.log('Something went wrong!', err);
-        });
+    }, function (err) {
+      console.log('Something went wrong!', err);
+    });
 });
 
 router.get('/rooms/:id', function (req, res, next) {
 
-    currentRoomSongs = storage.rooms[req.params.id]
+  currentRoomSongs = storage.rooms[req.params.id]
 
-    songs = []
+  songs = []
 
-    spotify.spotifyApi.getMyCurrentPlaybackState({})
-        .then(function (data) {
-            // Output items
-            console.log("Now Playing: ", data.body);
+  spotify.spotifyApi.getMyCurrentPlaybackState({})
+    .then(function (data) {
+      // Output items
+      console.log("Now Playing: ", data.body);
 
-            var currentId;
-            var currentSong;
+      var currentId;
+      var currentSong;
 
-            if (data.body.item === undefined) {
-                currentId = "";
-                currentSong = "";
-            } else {
-                currentId = data.body.item.id;
-                currentSong = data.body.item.name + " - " + data.body.item.artists[0].name
-            }
-
-
-            for (var key in currentRoomSongs) {
-                // check if the property/key is defined in the object itself, not in parent
-                if (currentRoomSongs.hasOwnProperty(key)) {
-                    if (currentRoomSongs[key].songID == currentId) {
-                        currentRoomSongs[key].votes = 0;
-                    }
-
-                    songs.push(currentRoomSongs[key])
-                }
-            }
-
-            songs.sort(compare);
-            console.log(songs);
-
-            res.render('voting', {
-                title: 'Oort',
-                roomID: req.params.id,
-                songLists: songs,
-                currentSong: currentSong
-            });
-
-            idx = setInterval(function () {
-                if (storage.onChange == 1) { // I guess you wanted a check for STATUS instead of VAR
-                    return clearInterval(idx);
-                }
-            }, 3000); // the interval when to call the function again 3000ms = 3sek
-            storage.onChange = 0;
-            res.redirect('back');
-        }, function (err) {
-            console.log('Something went wrong!', err);
-            res.redirect('/')
-        });
+      if (data.body.item === undefined) {
+        currentId = "";
+        currentSong = "";
+      } else {
+        currentId = data.body.item.id;
+        currentSong = data.body.item.name + " - " + data.body.item.artists[0].name
+      }
 
 
-    //while (storage.onChange == 0) { };
-    //storage.onChange = 0;
-    //res.redirect('back');
+      for (var key in currentRoomSongs) {
+        // check if the property/key is defined in the object itself, not in parent
+        if (currentRoomSongs.hasOwnProperty(key)) {
+          if (currentRoomSongs[key].songID == currentId) {
+            currentRoomSongs[key].votes = 0;
+          }
+
+          songs.push(currentRoomSongs[key])
+        }
+      }
+
+      songs.sort(compare);
+      console.log(songs);
+
+      res.render('voting', {
+        title: 'Oort',
+        roomID: req.params.id,
+        songLists: songs,
+        currentSong: currentSong
+      });
+
+      idx = setInterval(function () {
+        if (storage.onChange == 1) { // I guess you wanted a check for STATUS instead of VAR
+          return clearInterval(idx);
+        }
+      }, 3000); // the interval when to call the function again 3000ms = 3sek
+      storage.onChange = 0;
+      res.redirect('back');
+    }, function (err) {
+      console.log('Something went wrong!', err);
+      res.redirect('/')
+    });
+
+
+  //while (storage.onChange == 0) { };
+  //storage.onChange = 0;
+  //res.redirect('back');
 
 
 });
@@ -188,11 +196,11 @@ function saveDictToPublicFolder(fileData, callback) {
 */
 
 function compare(a, b) {
-    if (a.votes < b.votes)
-        return 1;
-    if (a.votes > b.votes)
-        return -1;
-    return 0;
+  if (a.votes < b.votes)
+    return 1;
+  if (a.votes > b.votes)
+    return -1;
+  return 0;
 }
 
 
